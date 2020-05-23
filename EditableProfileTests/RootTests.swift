f4677cd84c5a2b5b03b8d858d80327099e20b833
Aliaksei Zhemblouski <aliaksei.zhemblouski@viber.com>
2020-05-23 23:38:46 +0300

    Add root scene tests

--- EditableProfileTests/RootTests.swift
+++ EditableProfileTests/RootTests.swift
@@ -13,30 +13,79 @@ import Nimble
 final class RootTests: QuickSpec {
 
 	override func spec() {
-//		var rootViewModel: RootViewModel!
-//		var container: Container!
-//		
-//		beforeEach {
-//			container = Container()
-////			container.register(RootViewModel.self) { r in
-////				return RootViewModelImpl(userModelProvider: <#T##UserModelProvider#>, router: <#T##RootRouter#>)
-////			}
-//		}
-//		
-//		describe("User Model behavior") {
-//			
-//			context("When register or editing") {
-//			
-//				it("Should be nil") {
-//					let settings = container.resolve(UserModelProviderSettings.self)!
-//					expect(settings.userData).to(equal(nil))
-//				}
-//			
-//				it("Should be not nil") {
-//					let settings = container.resolve(UserModelProviderSettings.self)!
-//					expect(settings.userData).toNot(equal(nil))
-//				}
-//			}
-//		}
+		var rootVCMock: RootVCMock!
+		var userModelProviderMock: UserModelProviderMock!
+		var container: Container!
+		
+		beforeEach {
+			container = Container()
+			rootVCMock = RootVCMock()
+			userModelProviderMock = UserModelProviderMock()
+
+			container.register(RootViewModel.self) { r in
+				let model = RootViewModelImpl(userModelProvider: userModelProviderMock,
+											  router: RootRouterMock())
+				model.view = rootVCMock
+				return model
+			}
+		}
+		
+		describe("Root buttons appereance") {
+			context("User unregistered") {
+				beforeEach {
+					userModelProviderMock.isUserRegistered = false
+				}
+				
+				it("show only register button") {
+					let model = container.resolve(RootViewModel.self)!
+					model.viewWillAppear()
+					
+					expect(rootVCMock.registerButton != nil).to(equal(true))
+					expect(rootVCMock.changeProfileButton != nil).to(equal(true))
+					
+					expect(rootVCMock.changeProfileButton?.isHidden).to(equal(true))
+				}
+			}
+				
+			context("User registered") {
+				beforeEach {
+					userModelProviderMock.isUserRegistered = true
+				}
+				
+				it("Show both") {
+					let model = container.resolve(RootViewModel.self)!
+					model.viewWillAppear()
+					
+					expect(rootVCMock.registerButton != nil).to(equal(true))
+					expect(rootVCMock.changeProfileButton != nil).to(equal(true))
+					
+					expect(rootVCMock.changeProfileButton?.isHidden).to(equal(false))
+				}
+			}
+		}
+	}
+}
+
+fileprivate struct RootRouterMock: RootRouter {
+	func presentUserProfileViewController() { }
+}
+
+fileprivate final class UserModelProviderMock: UserModelProvider {
+	var isUserRegistered: Bool = false
+	
+	func fetchUserModel() -> UserModel? {
+		return nil
+	}
+	
+	func syncUserModel(_ model: UserModel?) { }
+}
+
+fileprivate final class RootVCMock: RootView {
+	var registerButton: RootButtonModel?
+	var changeProfileButton: RootButtonModel?
+	
+	func setupButtons(register: RootButtonModel, changeProfile: RootButtonModel) {
+		registerButton = register
+		changeProfileButton = changeProfile
 	}
 }
